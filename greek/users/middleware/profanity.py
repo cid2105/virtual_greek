20247e9b1ded99e052ce8a032d99b3a809a67226
Cole Diamond <cd1am0nd@yahoo.com>
2011-10-06 00:12:01 -0400

    asdf

--- greek/users/middleware/profanity.py
+++ greek/users/middleware/profanity.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 class ProfanityFilterMiddleware(object):
     def process_request(self, request):
-		setattr(request, '_dont_enforce_csrf_checks', True)
+        setattr(request, '_dont_enforce_csrf_checks', True)
         rpd = request.raw_post_data.lower()
         for w in settings.PROFANITIES_LIST:
             if rpd.find(w)!=-1:
