c18f2ae5d6cfbe19f9c25c6d62b33f89839ba3ff
Cole Diamond <cd1am0nd@yahoo.com>
2011-10-05 23:56:40 -0400

    sdf

--- greek/settings.py
+++ greek/settings.py
@@ -143,7 +143,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 "django.core.context_processors.media",
 "django.core.context_processors.static",
 "django.contrib.messages.context_processors.messages",
-	'views.default_context',
+#	'views.default_context',
 	'django_facebook.context_processors.facebook'
 )
 
