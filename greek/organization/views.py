18d77ada6e88e4a050e9547249d6495a4b384809
Cole Diamond <cd1am0nd@yahoo.com>
2011-10-06 00:50:01 -0400

    asdf

--- greek/organization/views.py
+++ greek/organization/views.py
@@ -53,7 +53,8 @@ def getDict(request, uni_name, org_name, Title=True):
 		chapter = Chapter(organization=org, university = uni)
 		chapter.save()
 	dict = {'base_url': base_url, 'uni_name': uni_name, 'org_name':org_name, 'org':org, 'uni':uni, 'title':title, 'hash_tags':getHashes(), 'chapter':chapter}
-	dict = paginateCollection(request, dict, Announcement.objects.filter(university = uni, organization=org), 'announcements')
+	if Announcement.objects.filter(university = uni, organization=org):
+		dict = paginateCollection(request, dict, Announcement.objects.filter(university = uni, organization=org), 'announcements')
 	return dict
 
 @csrf_exempt
